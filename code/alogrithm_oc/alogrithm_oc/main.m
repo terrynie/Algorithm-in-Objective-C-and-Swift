bd0695c32d386c277937f6823210c72384edd01b
terrynie <519046658@qq.com>
2016-01-24 23:31:30 +0800

    fix String

--- code/alogrithm_oc/alogrithm_oc/main.m
+++ code/alogrithm_oc/alogrithm_oc/main.m
@@ -10,8 +10,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *test = @"Hello World!";
+        NSString *test = @"Hello World! 5";
         NSLog(@"%lu",(unsigned long)test.length);
+        NSLog(@"%lu",(unsigned long)[test length]);
+        NSString *second = [NSString stringWithFormat:@"Hello World! %d",5];
+        if ([test isEqual:second]) {
+            NSLog(@"%@ is equal to %@",test,second);
+        }
+        
+        if ([test isEqualToString:second]) {
+            NSLog(@"%@ is equal to %@",test,second);
+        }
+        [test bo ]
+        
+        
     }
     return 0;
 }
